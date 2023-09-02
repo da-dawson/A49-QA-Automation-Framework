b25e7363476663eb9dd797bd27cb17bccff2f48e
Dominica <dominica.dawson@gmail.com>
2023-09-01 19:35:20 -0500

    debugging hw 21.

--- src/test/java/Homework21.java
+++ src/test/java/Homework21.java
@@ -5,11 +5,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class Homework21  extends BaseTest {
     String newPlaylistName = "Homework21";
 
     @Test
-    public void renamePlaylist()  {
+    public void renamePlaylist() throws InterruptedException {
         String updatedPlaylistMsg = "Updated playlist \"Homework21.\"";
 
         navigateToPage();
@@ -17,8 +18,9 @@ public class Homework21  extends BaseTest {
         enterPassword("Te$t$tudent");
         clickSubmit();
         doubleClickPlaylist();
-        enterNewPlaylistName();
-        Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatedPlaylistMsg);
+        Thread.sleep(2000);
+      //  enterNewPlaylistName();
+      //  Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatedPlaylistMsg);
     }
 
     public void doubleClickPlaylist() {
@@ -26,15 +28,7 @@ public class Homework21  extends BaseTest {
         actions.doubleClick(playlistElement).perform();
     }
 
-    public void enterNewPlaylistName() {
-        WebElement playlistInputField = driver.findElement(By.cssSelector("[name='name']"));
-        //clear() does not work, element has an attribute of "required", workaround is ctrl A (to select all)
-        // then backspace to clear then replace with new playlist name
-        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
 
-        playlistInputField.sendKeys(newPlaylistName);
-        playlistInputField.sendKeys(Keys.ENTER);
-    }
 
     public String getRenamePlaylistSuccessMsg() {
         WebElement notificationMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
