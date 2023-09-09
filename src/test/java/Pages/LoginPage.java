92cdbb609b895228235a1b764775e046c8958854
Dominica <dominica.dawson@gmail.com>
2023-09-09 17:23:15 -0500

    POM Homework.

---
+++ src/test/java/Pages/LoginPage.java
@@ -0,0 +1,28 @@
+package Pages;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+public class LoginPage extends BasePage{
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+    public void loginCorrectCred() {
+        provideEmail("dominica.dawson@testpro.io");
+        providePassword("te$t$tudent");
+        clickSubmit();
+    }
+
+
+    public void provideEmail(String email) {
+        enterText(By.cssSelector("input[type='email']"),email);
+    }
+
+    public void providePassword(String password) {
+        enterText(By.cssSelector("input[type='password']"),password);
+    }
+
+    public void clickSubmit() {
+        clickOnElement(By.cssSelector("[type='submit']"));
+    }
+}
